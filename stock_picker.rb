18b7419fcbc9ea54c1311d88eee40a00982d35d4
rhys <rbrooker105@gmail.com>
2016-08-05 21:43:13 +1000

    Code complete. Working program.

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,7 +1,21 @@
 def stock_picker stock_prices
+  max_profit = 0
   for buy in 0..stock_prices.length
-    p stock_prices[buy]
+    i = buy + 1
+	for sell in i..stock_prices.length
+	  profit = stock_prices[sell].to_i - stock_prices[buy].to_i
+	  if profit > max_profit
+		max_profit = profit
+		trading_days = [buy, sell]
+	  end
+	end
   end
+  puts "Buy on day #{trading_days[0]}, sell on day #{trading_days[1]} for a profit of $#{max_profit}"
+  "[#{trading_days[0]},#{trading_days[1]}]"
 end
 
-stock_picker([17,3,6,9,15,8,6,1,10])
+puts "Enter your stock prices on each hypothetical day, seperated by commas(,):"
+user_prices = gets.chomp.split(',').map { |price| price.to_i }
+puts stock_picker(user_prices)
+
+
